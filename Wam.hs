ca591d85d1a109cbe82517edb10382058f2bb296
Eli <eli@elilies.com>
2010-11-01 14:32:14 -0400

    IArray initial query compiling

--- Wam.hs
+++ Wam.hs
@@ -1,11 +1,12 @@
 module Main where
     
 import Data.List
-import Data.Array.IO
+--import Data.Array.IO
+import Data.Array.IArray
 
 type Func   = (String, Int)
 type Struct = (Func, [Term])
-type Var    = (String, Int)
+type Var    = String
 type Const  = String
 
 data Term   = V Var | C Const | S Struct deriving (Eq)
@@ -13,32 +14,41 @@ data Clause = Struct :- [Term]           deriving (Eq)
 
 data Cmd = Assertion Clause | Query [Term]
 
-data Cell = REF Int | STR Int | FUNCTOR Func
+data Cell = REF Int | STR Int | FUN Func | NULL
 
 -- Heap has an array and its current index (H)
-type Heap = ((IO (IOArray (Int, Int) Cell)), Int)
+type Heap = ((Array Int Cell), Int)
 
 instance Show Term where
-    show (V (v, n))  = v++(show n)
-    show (C c)       = c
+    show (V v)  		 = v
+    show (C c)       	 = c
     show (S ((f,a), ts)) = f ++ "/" ++ (show a) ++ "(" ++ (show ts) ++ ")"
-    showList x       = showString (intercalate ", " (map show x))
+    showList x       	 = showString (intercalate ", " (map show x))
     
 instance Show Clause where
     show (s :- ts) = (show (S s)) ++ " :- " ++ (show ts)
 
+instance Show Cell where
+	show (REF i) = "REF " ++ (show i)
+	show (STR i) = "STR " ++ (show i)
+	show (FUN f) = "FUN " ++ (show f)
+	show NULL    = "NULL"
 
-putStructure :: Heap -> Functor -> (Int, Heap)
--- putStructure heap functor = heap[H] <- STR H+1, heap[H+1] <- functor, H += 2, H-2
+getHeap :: Heap
+getHeap = (array (1, 1024) (zip [1..1024] $ repeat NULL), 1)
 
-setVariable :: Heap -> (Int, Heap)
--- setVariable heap = heap[H] <- REF H, H++, H-1
+putStructure :: Heap -> Func -> (Heap, Int)
+putStructure (heap, h) fnc = ((heap//[(h, STR (h+1)), (h+1, (FUN fnc))], h+2), h)
 
-setValue :: Heap -> Int -> Heap
--- setValue heap val = heap[H] <- val, H++        
+setVariable :: Heap -> (Heap, Int)
+setVariable (heap, h) = (((heap // [(h, REF h)]), h+1), h)
 
-main = do arr <- getArray
-          a   <- readArray arr 1
-          writeArray arr 1 5
-          b   <- readArray arr 1
-          print (a,b)
+setValue :: Heap -> Cell -> Heap
+setValue (heap, h) val = (heap // [(h, val)], h+1)
+
+compileQuery :: Heap -> Term -> Heap
+compileQuery heap (V _) 			  = fst $ setVariable heap
+compileQuery heap (C c)				  = fst $ putStructure heap (c, 0)
+compileQuery heap (S ((n, a), terms)) = foldl (\h q -> compileQuery h q) heap' terms
+										where (heap', _) = putStructure heap (n, a)
+
