17961de592ccc5fb301fafaac89f10842f445b2c
Eli <eli@elilies.com>
2010-12-02 20:49:46 -0500

    Program term flattening

--- wam.rb
+++ wam.rb
@@ -9,7 +9,54 @@ class WAM
     @pdl   = []
     @args  = []
   end
+  
 end
 
 EnvFrame = Struct.new(:ce, :cp, :vars)
 ChoiceFrame = Struct.new(:n, :args, :ce, :cp, :b, :bp, :tr, :h, :b0)
+
+Var = String
+
+Str = Struct.new(:name, :args) do
+  def arity
+    args.size
+  end
+
+  def to_s
+    "#{name}(#{args.join(', ')})"
+  end
+  
+  def inspect
+    to_s
+  end
+end
+
+def term?(t)
+  Var === t or Str === t
+end
+
+module Registers
+  class << self
+    
+    def flatten_program_term(p)
+      flat = flatten_term p
+      flat.map {|x| ref_term flat, x }
+    end
+  
+    private
+  
+    def ref_term(ts, t)
+      t.args.map! {|x| ts.index(x) } if Str === t
+      t
+    end
+  
+    def flatten_term(p)
+      case p
+        when Var then [p]
+        when Str then ([p] + p.args.map{|x| flatten_term x}).flatten.uniq
+        else          raise "Invalid term: #{p}"
+      end
+    end
+  
+  end
+end
