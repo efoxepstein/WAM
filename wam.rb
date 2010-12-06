8f1daf7fe19058756071727c10c4f15746dee59d
Eli <eli@elilies.com>
2010-12-06 14:26:31 -0500

    Compiling from registers calls right functions

--- wam.rb
+++ wam.rb
@@ -1,6 +1,9 @@
+require 'set'
+
 class WAM
-  attr_accessor :code, :heap, :stack, :trail, :pdl, :args
-  attr_accessor :p, :cp, :s, :hb, :b0, :b, :e
+  attr_accessor :code, :heap, :stack, :trail, :pdl, :args,
+                :p, :cp, :s, :hb, :b0, :b, :e, :mode, :seen
+  
   def initialize
     @code  = []
     @heap  = []
@@ -8,8 +11,39 @@ class WAM
     @trail = []
     @pdl   = []
     @args  = []
+    
+    mode = :write
   end
   
+  def compile_from_registers(regs)
+    @seen = Set.new
+    regs.each {|r| compile_register r }
+  end
+  
+  def compile_register(reg)
+#    puts "compile_register(#{reg.class.name})"
+    if Str === reg
+      get_structure reg.name
+      reg.args.each {|r| compile_register r }
+    elsif @seen.include? reg
+      unify_value reg
+    else
+      @seen << reg
+      unify_variable reg
+    end
+  end
+  
+  def get_structure(reg)
+    puts "get_structure #{reg}"
+  end
+  
+  def unify_variable(reg)
+    puts "unify_variable #{reg}"
+  end
+  
+  def unify_value(reg)
+    puts "unify_value #{reg}"
+  end
 end
 
 EnvFrame = Struct.new(:ce, :cp, :vars)
@@ -17,13 +51,35 @@ ChoiceFrame = Struct.new(:n, :args, :ce, :cp, :b, :bp, :tr, :h, :b0)
 
 Var = String
 
-Str = Struct.new(:name, :args) do
+class Str
+  attr_accessor :args, :name
+  
+  def self.[](*args)
+    new(*args)
+  end
+  
+  def initialize(nombre, blargs = [])
+    self.name = nombre
+    self.args = blargs
+  end
+  
   def arity
     args.size
   end
 
   def to_s
-    "#{name}(#{args.join(', ')})"
+    args.empty? ? name : "#{name}(#{args.map(&:to_s).join(', ')})"
+  end
+  
+  def eql?(str)
+    args === str.args and name === str.name
+  end
+  
+  def equal?(x) ; eql?(x) ; end
+  def ==(x) ; eql?(x) ; end
+  
+  def hash
+    [name, args].hash
   end
   
   def inspect
@@ -42,21 +98,29 @@ module Registers
       flat = flatten_term p
       flat.map {|x| ref_term flat, x }
     end
-  
-    private
-  
+    
     def ref_term(ts, t)
-      t.args.map! {|x| ts.index(x) } if Str === t
-      t
+      if Str === t
+        Str[t.name, t.args.map {|x| ts.index(x) } ]
+      else
+        t.clone
+      end
     end
-  
+    
     def flatten_term(p)
       case p
-        when Var then [p]
-        when Str then ([p] + p.args.map{|x| flatten_term x}).flatten.uniq
+        when Var then [p.clone]
+        when Str
+          args = p.args.map{|x| flatten_term x}
+          ([p.clone] + args).flatten.uniq
         else          raise "Invalid term: #{p}"
       end
     end
   
   end
 end
+
+# f(x, g(x), x) ==> [f(1, 2, 1), "x", g(1)]
+GOOD_TERM = Str.new('f', ['x', Str.new('g',['x']), 'x'])
+
+ADD_FACT = Str['add', [Str['o'], Str['s', [Str['o']]], Str['s', [Str['o']]]]]
