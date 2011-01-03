1e66e09810b2242d2591449ab9fc4be8fbf0b742
Eli <eli@elilies.com>
2011-01-02 23:50:38 -0500

    Proper program term compilation

--- Wam.hs
+++ Wam.hs
@@ -97,6 +97,7 @@ getCell db (REGS idx) = (fst $ regs db) ! idx
 
 -- puts a cell at an address
 putCell :: Db -> Address -> Cell -> Db
+--putCell _ a c | trace ("putCell " ++ show c ++ " into " ++ show a) False = undefined
 putCell db@(Db {heap=(h', h)}) (HEAP i) cell = db {heap=(h' // [(i, cell)], h)} 
 putCell db@(Db {code=(h', h)}) (CODE i) cell = db {code=(h' // [(i, cell)], h)} 
 putCell db@(Db {regs=(h', h)}) (REGS i) cell = db {regs=(h' // [(i, cell)], h)} 
@@ -109,6 +110,7 @@ incrAddr (REGS i) = REGS (i+1)
 
 -- puts a cell on top of the heap
 pushOnHeap :: Heap -> Cell -> Heap
+--pushOnHeap h c | trace ("pushHeap: " ++ show c) False = undefined
 pushOnHeap (heap, idx) cell = (heap // [(idx, cell)], idx + 1)
 
 
@@ -185,7 +187,7 @@ setVariable db@(Db {code=code, regs=regs}) addr =
 -- compiles a query term structure into the heap
 -- defined on pg 14, fig 2.2
 putStructure :: Db -> (Func, [Int]) -> Address -> Db
-putStructure _ (f,_) _ | trace ("putStructure " ++ (show f)) False = undefined
+--putStructure _ (f,_) _ | trace ("putStructure " ++ (show f)) False = undefined
 putStructure db@(Db {code=code, regs=regs}) (f, args) addr =
     let h     = 1 + (snd code)
         code1 = pushOnHeap code  (STR (CODE h))
@@ -193,7 +195,26 @@ putStructure db@(Db {code=code, regs=regs}) (f, args) addr =
         db1   = putCell db addr (STR (CODE h))
     in  db1 { code = code2 }
 
-
+-- So: (write mode)
+--     unifyVariable:
+--         (A) puts a self-referential REF when we encounter the g in f(g)
+--         (B) puts a ref to that self-ref into the register
+--     then getStructure:
+--         makes a new str (C) and fun at top of CODE
+--         encounters the REF (&A)
+--         binds it to the str (&C)
+--     thus, we end up with:
+--         B points to A
+--         A points to C
+--         C is intact
+-- What's not happening:
+--     A is not being bound to point at C
+--     Whereas, B is being bound
+--     THUS, I suspect that deref isn't working
+--     In fact, that is the case because we can see the bind call there is erroneous
+--     That should be REF CODE 2 (&A)
+--     But wait! The value at REGS 2 is (REF REGS 2) when it really should be (REF CODE 2)
+--     So something before the deref call is erroneous, look:
 
 
 -- ------------------------ --
@@ -205,12 +226,12 @@ compileProgramTerm db term =
     fst $ foldl compileRefTerm (db, []) (flattenTerm term)
 
 -- does the actual compilation of a flattened term
--- TODO: this has some other responsibilities that it's not doing yet
+-- TODO: this has some other responsibilities that it's not doing yet.. hmmm
 compileRefTerm :: (Db, [Int]) -> RefTerm -> (Db, [Int])
 compileRefTerm (db, idxs) (RefS (f, is)) =
-    let db'  = getStructure db f
+    let db'  = getStructure db f (REGS (snd $ regs db))
     in foldl unifyVarVal (db', idxs) is
-compileRefTerm x _ = x
+compileRefTerm (db@(Db {regs=(r,i)}), idxs) _ = (db {regs=(r,i+1)}, idxs)
 
 -- picks unifyValue or unifyVariable depending on whether or not
 -- we've already seen the variable
@@ -222,41 +243,41 @@ unifyVarVal (db, idxs) idx | elem idx idxs = (unifyValue db idx, idxs)
 -- (when we've already seen it and compiled it with unifyVariable)
 -- defined on pg 18, fig 2.6
 unifyValue :: Db -> Int -> Db
-unifyValue _ _ | trace ("unifyValue") False = undefined
-unifyValue db@(Db {mode=READ, s=s}) i = db `fromMaybe` (unify db (REGS i) s)
+--unifyValue db@(Db {mode=m}) i | trace ("UNIFYVALUE: " ++ show (m,i)) False = undefined
+unifyValue db@(Db {mode=READ, s=s}) i = unify db (REGS i) s
 unifyValue db@(Db {code=code, s=s}) i = 
-    let code' = pushOnHeap code (REF (CODE i))
+    let code' = pushOnHeap code $ getCell db (REGS i) -- (REF (CODE i))
     in  db { code = code', s = incrAddr s }
 
 -- compiles a program term variable into the heap
 -- (when we haven't seen it before)
 -- defined on pg 18, fig 2.6
 unifyVariable :: Db -> Int -> Db
-unifyVariable _ _ | trace ("unifyVariable") False = undefined
+--unifyVariable db@(Db {mode=m}) i | trace ("UNIFYVARIABLE: " ++ show (m, i)) False = undefined
 unifyVariable db@(Db {mode=READ, s=s}) i =
     let cell = getCell db s
         db'  = putCell db (REGS i) cell
     in  db' { s = (incrAddr s) }
 unifyVariable db@(Db {code=code, s=s, regs=regs}) i =
     let h   = snd code
-        db1 = db  { code = pushOnHeap code (REF (CODE h)) }
-        db2 = db1 { regs = pushOnHeap regs (REF (CODE h)) }
-    in db2 { s = (incrAddr s) }
-
+        db2 = db { code = pushOnHeap code (REF (CODE h)), s = incrAddr s }
+    in  putCell db2 (REGS i) (REF (CODE h))
+        
 -- compiles a program term structure into the heap
 -- defined on pg 18, fig 2.6   
-getStructure :: Db -> Func -> Db
-getStructure db f = getStructure' db f $ getCell db $ deref db (REGS (snd $ regs db))
+getStructure :: Db -> Func -> Address -> Db
+--getStructure _ f addr | trace ("GETSTRUCTURE: " ++ show f ++ "\t" ++ show addr) False = undefined
+getStructure db f addr = getStructure' db f $ getCell db $ deref db addr
+
 
 -- helper for getStructure
 getStructure' :: Db -> Func -> Cell -> Db
-getStructure' _ _ _ | trace ("getStructure'") False = undefined
+--getStructure' _ f c  | trace ("GETSTRUCTURE': " ++ show f ++ "\t" ++ show c) False = undefined
 getStructure' db@(Db {code=code, regs=(r,i)}) f (REF addr) =
     let code1 = pushOnHeap code  (STR (CODE (1 + (snd code))))
         code2 = pushOnHeap code1 (FUN f)
-        db1 = db  {mode = WRITE, code = code2}
-        db2 = db1 {regs = (r,i+1) }
-    in  bind db2 addr (CODE ((snd code2) - 2)) 
+        db1   = db {code=code2, mode = WRITE, regs=(r,i+1)}
+    in  bind db1 addr (CODE (snd code))
 getStructure' db@(Db {code=code}) f (STR addr) 
     -- | trace (show cell) False = undefined
     | isFun cell = db { s = incrAddr addr, mode = READ } 
@@ -269,7 +290,6 @@ getStructure' db@(Db {code=code}) f (STR addr)
 -- address in the database
 -- defined on pg 17, fig 2.5   
 deref :: Db -> Address -> Address
---deref db adr | trace ("deref\t" ++ show adr) False = undefined
 deref db adr | isSelfRef db cell = adr
              | isRef cell        = deref db $ (\(REF x) -> x) cell
              | otherwise         = adr
@@ -287,7 +307,7 @@ isSelfRef _ _                                  = False
 -- does 'bind' (TODO: expand on this explanation)
 -- defined on pg 113
 bind :: Db -> Address -> Address -> Db
---bind db a1 a2 | trace (show a1 ++ "\n" ++ show a2) False = undefined
+--bind db a1 a2 | trace ("bind: " ++ show a1 ++ "\t" ++ show a2) False = undefined
 bind db a1 a2 =
     let cell1 = getCell db a1
         cell2 = getCell db a2
@@ -295,8 +315,9 @@ bind db a1 a2 =
 
 -- does the actual work of binding
 bindHelper :: Db -> (Cell, Address) -> (Cell, Address) -> Db
+--bindHelper _ x y
+--    | trace ("bind: " ++ show x ++ "\t" ++ show y) False = undefined
 bindHelper db (REF addr, a1) (cell2, a2)
---    | trace (show addr ++ "\t" ++ show cell2) False = undefined
     | (not $ isRef cell2) || (a2 `addrLt` a1) = 
         let db' = putCell db a1 cell2
         in  trail db' a1
@@ -361,41 +382,52 @@ refTerm ts (S (f, subs)) = RefS (f, mapMaybe (\x->elemIndex x ts) subs)
 -- UNIFICATION --
 -- ----------- --
 
-unify :: Db -> Address -> Address -> Maybe Db
-unify db a1 a2 = unify' db [a2, a1] -- use list as a stack, TOS is head
+unify :: Db -> Address -> Address -> Db
+unify db a1 a2 = db `fromMaybe` unify' db [a2, a1] -- use list as a stack, TOS is head
+
+
+-- What?
 
 unify' :: Db -> [Address] -> Maybe Db
+unify' _ as | trace ("unify': " ++ show as) False = undefined
 unify' db (a1 : a2 : addrs) =
-    let d1            = deref db a1
-        d2            = deref db a2
-        (db2, addrs') = unifyTags db d1 d2
-    in  db2 >>= (\x -> unify' x (addrs' ++ addrs))
+    let (d1, d2)      = (deref db a1, deref db a2)
+        (db2, addrs') = unifyTags db d1 d2         -- <-- this does the bulk of the work
+    in  db2 >>= (\x -> unify' x (addrs' ++ addrs)) -- <-- this is the while loop
 unify' db _ = Just db
     
+-- If the two addresses point to REFs, then bind the first to the second
+-- Otherwise, they should be REFs that point to FUNs. If not, we're in trouble...
 unifyTags :: Db -> Address -> Address -> (Maybe Db, [Address])
+unifyTags _ _ _ | trace ("unifyTags") False = undefined
 unifyTags db a1 a2 | a1 == a2  = (Just db, [])
                    | otherwise =
-    let c1 = getCell db a1
-        c2 = getCell db a2
+    let c1 = trace ("\tCell1: " ++ show (getCell db a1)) (getCell db a1)
+        c2 = trace ("\tCell2: " ++ show (getCell db a2)) (getCell db a2)
     in if (isRef c1) || (isRef c2)
        then (Just (bind db a1 a2), [])
        else unifyFunctors db (c1, a1) (c2, a2)
 
+-- Each cell should be a STR that points to a FUN.
 unifyFunctors :: Db -> (Cell, Address) -> (Cell, Address) -> (Maybe Db, [Address])
-unifyFunctors db (REF a, aAddr) (REF b, bAddr) =
-    let a' = getCell db a
-        b' = getCell db b
+unifyFunctors _ a b | trace ("unifyFunctors: " ++ show a ++ "\t" ++ show b) False = undefined
+unifyFunctors db (STR a, aAddr) (STR b, bAddr) =
+    let a' = trace ("\tCell1: " ++ show (getCell db a)) (getCell db a)
+        b' = trace ("\tCell2: " ++ show (getCell db b)) (getCell db b)
     in if a' == b'
-       then (Just db, takeCells db aAddr bAddr)
+       then (Just db, takeCells db (incrAddr aAddr) (incrAddr bAddr))
        else (Nothing, [])
 
 takeCells :: Db -> Address -> Address -> [Address]
+takeCells _ a1 a2 | trace ("takeCells: " ++ show (a1, a2)) False = undefined
 takeCells db a1 a2 =
-    case (getCell db a1) of
-        (FUN (_, arity)) -> takeCells' db arity (incrAddr a1) (incrAddr a2)
+    case traceShowRet (getCell db a1) of
+        (FUN (_, arity)) -> traceShowRet $ takeCells' db arity (incrAddr a1) (incrAddr a2)
         _                -> []
     where takeCells' _  0 _ _ = []
           takeCells' db x a b = (b : a : takeCells' db (x-1) (incrAddr a) (incrAddr b))
+
+traceShowRet x = traceShow x x
 
 ------- PARSING --------
 e2m :: Either ParseError a -> Maybe a
@@ -445,6 +477,7 @@ testTerm2 = p "p(f(X), h(Y, f(a)), Y)"
 testQuery = p "p(Z, h(Z, W), f(W))"
 
 printCode x = putStrLn $ intercalate "\n" $ map (\(y,z)->(show y) ++ "\t" ++ (show z)) $ zip [0..] $ getCode x
+printRegs x = putStrLn $ intercalate "\n" $ map (\(y,z)->(show y) ++ "\t" ++ (show z)) $ zip [0..] $ getRegs x
 
 getCode = elems . fst . code
 getRegs = elems . fst . regs
